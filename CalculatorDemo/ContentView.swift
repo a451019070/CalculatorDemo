f91d2049b8ba07c2878504461ae20da7548862f8
ZX <895843633@qq.com>
2020-03-09 13:22:34 +0800

    Update ContentView.swift
    添加按钮

--- CalculatorDemo/ContentView.swift
+++ CalculatorDemo/ContentView.swift
@@ -11,16 +11,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text("+")
-            .font(.system(size: 38))
-            .foregroundColor(.white)
-            .frame(width: 88, height: 88) .background(Color("operatorBackground"))
-            .cornerRadius(44)
+        HStack{
+            CalculatorButton(title: "1", size: CGSize(width: 88, height: 88), backgroundColorName: "digitBackground", action: {
+                print("Button:1")
+            })
+            CalculatorButton(title: "2", size: CGSize(width: 88, height: 88), backgroundColorName: "digitBackground", action: {
+                print("Button:2")
+            })
+            CalculatorButton(title: "3", size: CGSize(width: 88, height: 88), backgroundColorName: "digitBackground", action: {
+                print("Button:3")
+            })
+            CalculatorButton(title: "+", size: CGSize(width: 88, height: 88), backgroundColorName: "operatorBackground", action: {
+             print("Button:+")
+            })
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CalculatorButton: View {
+    let fontSize:CGFloat = 38
+    let title : String
+    let size : CGSize
+    let backgroundColorName : String
+    let action:() -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+                .frame(width: size.width, height: size.height) .background(Color(backgroundColorName))
+                .cornerRadius(size.width/2)
+        })
     }
 }
